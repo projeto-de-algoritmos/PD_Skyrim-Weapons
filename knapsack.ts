2eb6eadec7cf5a143cb8399abb60bf9dece9da25
Giulia Lobo <42353909+Giuulob89@users.noreply.github.com>
2023-02-06 11:13:01 -0300

    Correção de erros e adição da interface Weapon
    Co-authored-by: Matheus Pimentel <matheusp.eng@gmail.com>

--- knapsack.ts
+++ knapsack.ts
@@ -1,36 +1,47 @@
 import weaponData from './data/skyrim_weapons.json' assert { type: 'json' };
+interface Weapon {
+    "Name": string,
+    "Damage": number,
+    "Weight": number,
+    "Gold": number,
+    "Upgrade": string,
+    "Perk": string,
+    "Type": string,
+    "Category": string,
+    "Speed": string | number
+}
 
-function findOptimal (totalWeight: number): number {
+
+function findOptimal (totalWeight: number): [number, number[]] {
     //Criação do array 2x2 de tamanho fixo
-    let Matriz = new Array(weaponData.length+1);
+    let Matrix = new Array(weaponData.length+1);
 
-    for (let i; i <= weaponData.length; i++) {
-        Matriz[i] = new Array(totalWeight+1);
+    for (let i = 0; i <= weaponData.length; i++) {
+        Matrix[i] = new Array(totalWeight+1);
     }
 
     //Preenchimento da linha que contém 0 itens e que o peso disponível é 0
     for (let w = 0; w <= totalWeight; w++) {
-        Matriz[0][w] = 0;
+        Matrix[0][w] = 0;
     }
 
     for (let c = 1; c <= weaponData.length; c++) {
-        Matriz[c][0] = 0;
+        Matrix[c][0] = 0;
     }
 
     // Função Knapsack PD
     weaponData.map((weapon, index) => {
         for (let w = 1; w <= totalWeight; w++) {
             if (Math.floor(weapon.Weight) > w) {
-                Matriz[index+1][w] = Matriz[index][w];
+                Matrix[index+1][w] = Matrix[index][w];
             } else {
-                Matriz[index+1][w] = Math.max(
-                    Matriz[index][w], weapon.Gold + Matriz[index][w-Math.floor(weapon.Weight)]);
+                Matrix[index+1][w] = Math.max(
+                    Matrix[index][w], weapon.Gold + Matrix[index][w-Math.floor(weapon.Weight)]);
             }
         }
     })
     
-    // console.log(Matriz[weaponData.length][totalWeight]);
-    return Matriz[weaponData.length][totalWeight];
+    return Matrix[weaponData.length][totalWeight];
 }
 
 findOptimal(1);
